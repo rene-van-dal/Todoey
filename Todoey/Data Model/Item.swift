1e82b37a16326d0c0b0d12807f7ffc10be7b41aa
René van Dal <rene@ics-macbook-pro.home>
2018-04-08 11:41:35 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by René van Dal on 08/04/2018.
+//  Copyright © 2018 René van Dal. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
